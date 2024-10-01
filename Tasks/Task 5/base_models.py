9abb48796ef685e884dd35ef6c88c8345f51cfa4
Sid Pradhan <64797131+SiddharthaPradhan@users.noreply.github.com>
2024-10-01 19:40:06 -0400

    revamped preprocessing, one-hot-encoded for cnn, and label encoded for rest

--- Tasks/Task 5/base_models.py
+++ Tasks/Task 5/base_models.py
@@ -8,11 +8,13 @@
 from sklearn.naive_bayes import GaussianNB
 from load_data import load_train_data
 from sklearn.metrics import  accuracy_score
+import sys
 if __name__ == '__main__':
     # load data
     x, y = load_train_data()
-    gnb_clf = GaussianNB()
-    gnb_clf.fit(x,y)
-    print(gnb_clf.score(x,y))
+    print(sys.getsizeof(x)+sys.getsizeof(y))
+    # gnb_clf = GaussianNB()
+    # gnb_clf.fit(x,y)
+    # print(gnb_clf.score(x,y))
     # print(accuracy_score(y,gnb_clf.predict(x)))
 
--- Tasks/Task 5/load_data.py
+++ Tasks/Task 5/load_data.py
@@ -1,13 +1,14 @@
 from datasets import load_from_disk 
 
-SAVE_DIR = '../processed_data'
+SAVE_DIR = '../processed_data/'
 
 # return x (i.e image pixel values) and y (encoded label (i.e list or label value))
 def load_train_data(for_CNN=False, num_samples=None):
     if for_CNN:
-        pass # return data of shape (batch_size,64,64,3)
-    else: # return data of shape (n_rows, 64*64*3)
-        ds = load_from_disk(SAVE_DIR)
-        if num_samples != None:
-            ds = ds.select(range(num_samples))
-        return ds['img_pixels'], ds['y']
+        path += 'cnn' # shape = (HEIGHT, WIDTH)
+    else: 
+        path += 'regular' # shape = (HEIGHT*WIDTH)
+    ds = load_from_disk(SAVE_DIR)
+    if num_samples != None:
+        ds = ds.select(range(num_samples))
+    return ds['img_pixels'], ds['y']
