46d71162717f9a45275093351d546e2209c18c94
Sid Pradhan <64797131+SiddharthaPradhan@users.noreply.github.com>
2024-10-03 01:16:12 -0400

    fix train.sh

--- train.sh
+++ train.sh
@@ -7,7 +7,7 @@
 #SBATCH -p short
 #SBATCH -t 12:00:00
 #SBATCH --gres=gpu:2
-#SBATCH --output=/home/$USER/KDDProject2/train_svm-%j.txt
+#SBATCH --output=/home/sppradhan/KDDProject2/train_svm-%j.txt
 module load cuda
 module load python/3.10.13
 source ~/KDDProject2/kdd/bin/activate
