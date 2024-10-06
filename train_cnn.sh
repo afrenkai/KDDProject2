e9bac01efc98ca30970ee133d373ca333784a581
Sid Pradhan <64797131+SiddharthaPradhan@users.noreply.github.com>
2024-10-06 16:14:03 -0400

    push for turing

--- train_cnn.sh
+++ train_cnn.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -n 10
+#SBATCH -n 8
 #SBATCH --mem=100g
-#SBATCH -J "SVM test"
+#SBATCH -J "CNN test"
 #SBATCH -p short
 #SBATCH -t 12:00:00
-#SBATCH --gres=gpu:2
-#SBATCH --output=/home/sppradhan/KDDProject2/train_%j.txt
-module load cuda
+#SBATCH --gres=gpu:1
+#SBATCH --output=/home/sppradhan/KDDProject2/train_cnn_%j.txt
+module load cuda11.7
 module load python/3.10.13
 source ~/KDDProject2/kdd/bin/activate
 pip install -r requirements.txt
