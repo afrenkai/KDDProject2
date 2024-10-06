c42a03c77f07cdf5c8e53000edae5180e8a5a462
Sid Pradhan <64797131+SiddharthaPradhan@users.noreply.github.com>
2024-10-06 14:44:59 -0400

    push for turing

--- Tasks/Task 5/base_model_cnn.py
+++ Tasks/Task 5/base_model_cnn.py
@@ -5,16 +5,16 @@ from sklearn.metrics import  accuracy_score
 
 
 if __name__ == '__main__':
-    NUM_SAMPLES = 5000
+    NUM_SAMPLES = None
     # load data like this for batched
     train_dataset_batched, val_dataset, test_dataset, unique_styles = get_datasets(for_CNN=True, val_size=0.2, 
-                                                                              batch_size=64, num_samples=NUM_SAMPLES)
+                                                                              batch_size=32, num_samples=NUM_SAMPLES)
 
     
-    # get un-batched dataset
-    train_dataset, _, _, _ = get_datasets(for_CNN=False, val_size=0.2,batch_size=None, num_samples=NUM_SAMPLES)
+    # # get un-batched dataset
+    # train_dataset, _, _, _ = get_datasets(for_CNN=False, val_size=0.2,batch_size=None, num_samples=NUM_SAMPLES)
 
-    cnn_clf = CNNClassifier(train_dataset_batched, val_dataset, test_dataset, unique_styles, epochs=1)
+    cnn_clf = CNNClassifier(train_dataset_batched, val_dataset, test_dataset, unique_styles)
     cnn_clf.run()
 
 
