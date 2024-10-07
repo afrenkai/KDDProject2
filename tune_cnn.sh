7c1efe2553db887cba6b0b21c823f1cfdea06c01
Sid Pradhan <64797131+SiddharthaPradhan@users.noreply.github.com>
2024-10-06 22:48:34 -0400

    added tuning script

---
+++ tune_cnn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 8
+#SBATCH --mem=100g
+#SBATCH -J "CNN tune"
+#SBATCH -p short
+#SBATCH -t 16:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --output=/home/sppradhan/tune_cnn_%j.txt
+module load cuda
+module load python/3.10.13
+source ~/KDDProject2/kdd/bin/activate
+cd Tasks/Task\ 5/
+python tune_cnn.py
